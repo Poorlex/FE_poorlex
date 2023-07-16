62b90bba7ba3fc126f258eb67cd2109c48914138
leosister1 <yuuj22n@gmail.com>
2023-07-16 21:32:44 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/poorlex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poorlex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First app'),
+      ),
+    );
+  }
+}
