d436528159a3783efaf455ec27b1459ba81c3597
ethan <teajoon.work@gmail.com>
2023-12-06 21:18:58 +0900

    요약 페이지 퍼블리싱 진행중

---
+++ lib/Screen/calendar_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'package:poorlex/Widget/MainPageWidget/bottom_bar.dart';
+import 'package:poorlex/Widget/CalendarPage/header.dart';
+
+class CaledarPage extends StatefulWidget {
+  const CaledarPage({super.key});
+
+  @override
+  State<CaledarPage> createState() => _CaledarPageState();
+}
+
+class _CaledarPageState extends State<CaledarPage> {
+  int _current = DateTime.now().millisecondsSinceEpoch;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        bottomNavigationBar: const BottomBar(
+          nowPage: 3,
+        ),
+        body: (
+          Container(
+              padding: EdgeInsets.fromLTRB(0, MediaQuery.of(context).viewPadding.top + 20, 0, MediaQuery.of(context).viewPadding.bottom),
+              child: (
+                Expanded(
+                  child: Column(
+                    children: [
+                      CalenderHeader(this._current),
+                      SingleChildScrollView(
+                        child: (
+                            Expanded(
+                              child: (
+                                  Column(
+                                    children: [
+                                      Text(
+                                          '요약',
+                                          style: TextStyle(
+                                              color: Colors.white
+                                          )
+                                      )
+                                    ],
+                                  )
+                              )
+
+                            )
+                        )
+                      )
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                  )
+                )
+              )
+          )
+        )
+    );
+  }
+}
---
+++ lib/Widget/CalendarPage/header.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class CalenderHeader extends StatelessWidget {
+  final int current;
+
+  const CalenderHeader(
+      int this.current,
+      {
+        super.key,
+      }
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+      child: (
+        Expanded(
+          child: Column(
+              children: [
+                Text('요약', style: TextStyle(fontSize: 24, color: Colors.white)),
+                SizedBox(height: 20),
+                Container(
+                    child: Row(
+                        children: [
+                          IconButton(
+                              icon: Icon(Icons.arrow_back_ios, size: 25, color: Colors.white,),
+                              onPressed: () => print('111')
+                          ),
+                          TextButton(
+                              child: Row(
+                                children: [
+                                  Text(this.current.toString(), style: TextStyle(color: Colors.white, fontSize: 25)),
+                                  SizedBox(width: 8),
+                                  Icon(Icons.arrow_drop_down_sharp, size: 35, color: Colors.white,)
+                                ],
+                              ),
+                              onPressed: () => print('22222')
+                          ),
+                          IconButton(
+                              icon: Icon(Icons.arrow_forward_ios, size: 25, color: Colors.white,),
+                              onPressed: () => print('111')
+                          )
+                        ],
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    )
+                )
+              ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          )
+        )
+      )
+    );
+  }
+}
