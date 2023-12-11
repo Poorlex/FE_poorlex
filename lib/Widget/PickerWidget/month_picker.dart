86881f14bed4a7820057b06dc5b1a33751522768
ethan <teajoon.work@gmail.com>
2023-12-11 19:34:36 +0900

    요약 페이지 퍼블리싱 진행중 (달력 픽커)

--- lib/Widget/PickerWidget/month_picker.dart
+++ lib/Widget/PickerWidget/month_picker.dart
@@ -1,30 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_file.dart';
 import 'package:intl/intl.dart';
 
 class MonthPicker extends StatefulWidget {
   static final _now = DateTime.now().millisecondsSinceEpoch;
-  int current = _now;
-  bool isOpen = false;
+  int current;
   Widget child;
-  List<int> years;
-  List<int> months;
+  Function select;
 
   MonthPicker({
     super.key,
-    this.isOpen = false,
     int? current,
     Widget? child,
+    required this.select,
   }) :
       current = current ?? _now,
-      child = child ?? Text((current ?? _now).toString()),
-      years = List<int>.generate(20, (index) => index).map((index) {
-        int y = int.parse(DateFormat('yyyy').format(DateTime.fromMillisecondsSinceEpoch(_now)));
-        return y - index;
-      }).toList(),
-      months = List<int>.generate(12, (index) => index).map((index) {
-        return index + 1;
-      }).toList()
+      child = child ?? Text((current ?? _now).toString())
   {}
 
   @override
@@ -41,106 +32,7 @@ class _MonthPickerState extends State<MonthPicker> {
             showModalBottomSheet<void>(
               context: context,
               builder: (BuildContext context) {
-                return SizedBox(
-                  height: 300,
-                  child: (
-                    Container(
-                      color: Colors.white,
-                      padding: EdgeInsets.fromLTRB(20, 20, 20, 40),
-                      child: (
-                        Expanded(child: (
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(child: Row(
-                                  children: [
-                                    Expanded(
-                                      flex: 1,
-                                      child: ListView(
-                                        scrollDirection: Axis.vertical,
-                                        children: widget.years.map((year) {
-                                          bool isSelected = year.toString() == DateFormat('yyyy').format(DateTime.fromMillisecondsSinceEpoch(widget.current));
-                                          if (isSelected) {
-                                            return ElevatedButton(
-                                              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                                              child: Center(child: (
-                                                  Text(year.toString() + '년',
-                                                      style: TextStyle(fontSize: 16, color: Colors.white)
-                                                  )
-                                              )),
-                                              onPressed: () => print(111),
-                                            );
-                                          } else {
-                                            return TextButton(
-                                              child: Center(child: (
-                                                  Text(year.toString() + '년',
-                                                      style: TextStyle(fontSize: 16, color: Colors.black)
-                                                  )
-                                              )),
-                                              onPressed: () => print(111),
-                                            );
-                                          }
-                                        }).toList(),
-                                      ),
-                                    ),
-                                    /*
-                                            Container(
-                                            color: isSelected ? Colors.black : Colors.white,
-                                            padding: EdgeInsets.fromLTRB(0, 4, 0, 4),
-                                            child: Center(child: (
-                                              Text(year.toString() + '년',
-                                                  style: TextStyle(fontSize: 16, color: isSelected ? Colors.white : Colors.black)
-                                              )
-                                            )),
-                                             */
-                                    SizedBox(width: 8),
-                                    Expanded(
-                                      flex: 1,
-                                      child: ListView(
-                                        scrollDirection: Axis.vertical,
-                                        children: widget.months.map((month) {
-                                          bool isSelected = month.toString() == DateFormat('M').format(DateTime.fromMillisecondsSinceEpoch(widget.current));
-                                          if (isSelected) {
-                                            return ElevatedButton(
-                                              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                                              child: Center(child: (
-                                                  Text(month.toString() + '월',
-                                                      style: TextStyle(fontSize: 16, color: Colors.white)
-                                                  )
-                                              )),
-                                              onPressed: () => print(111),
-                                            );
-                                          } else {
-                                            return TextButton(
-                                              child: Center(child: (
-                                                  Text(month.toString() + '년',
-                                                      style: TextStyle(fontSize: 16, color: Colors.black)
-                                                  )
-                                              )),
-                                              onPressed: () => print(111),
-                                            );
-                                          }
-                                        }).toList(),
-                                      )
-                                    )
-                                  ],
-                                )),
-                                SizedBox(height: 16),
-                                Container(
-                                  child: (
-                                      ElevatedButton(
-                                          style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFFFD600), minimumSize: Size.fromHeight(50)),
-                                          child: Text('확인', style: TextStyle(color: Colors.black, fontSize: 18),),
-                                          onPressed: () => print(111)
-                                      )
-                                  ),
-                                ),
-                              ],
-                            )
-                        ))
-                    ),)
-                  )
-                );
+                return MonthPickerModal(current: widget.current, select: widget.select);
               },
             );
           }
@@ -148,3 +40,133 @@ class _MonthPickerState extends State<MonthPicker> {
     );
   }
 }
+
+class MonthPickerModal extends StatefulWidget {
+  int current;
+  Function select;
+
+  MonthPickerModal({
+    super.key,
+    required this.current,
+    required this.select
+  });
+
+  @override
+  State<MonthPickerModal> createState() => _MonthPickerModalState();
+}
+
+class _MonthPickerModalState extends State<MonthPickerModal> {
+  static final _now = DateTime.now().millisecondsSinceEpoch;
+  List<int> years;
+  List<int> months;
+
+  _MonthPickerModalState():
+        years = List<int>.generate(20, (index) => index).map((index) {
+          int y = int.parse(DateFormat('yyyy').format(DateTime.fromMillisecondsSinceEpoch(_now)));
+          return y - index;
+        }).toList(),
+        months = List<int>.generate(12, (index) => index).map((index) {
+          return index + 1;
+        }).toList()
+  {}
+
+  void _select(String mode, int n) {
+    DateTime c = DateTime.fromMillisecondsSinceEpoch(widget.current);
+    DateTime t;
+    if (mode == 'year') {
+      t = new DateTime(n, c.month, c.day, c.hour, c.minute, c.second, c.millisecond, c.microsecond);
+    } else {
+      t = new DateTime(c.year, n, c.day, c.hour, c.minute, c.second, c.millisecond, c.microsecond);
+    }
+    setState(() {
+      widget.current = t.millisecondsSinceEpoch;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 300,
+        child: (
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 40),
+              child: (
+                  Expanded(child: (
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(child: Row(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: ListView(
+                                  scrollDirection: Axis.vertical,
+                                  children: years.map((year) {
+                                    bool isSelected = year.toString() == DateFormat('yyyy').format(DateTime.fromMillisecondsSinceEpoch(widget.current));
+                                    if (isSelected) {
+                                      return ElevatedButton(
+                                        style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                                        child: Text(year.toString() + '년',
+                                            style: TextStyle(fontSize: 16, color: Colors.white)
+                                        ),
+                                        onPressed: () => _select('year', year),
+                                      );
+                                    } else {
+                                      return TextButton(
+                                        child: Text(year.toString() + '년',
+                                            style: TextStyle(fontSize: 16, color: Colors.black)
+                                        ),
+                                        onPressed: () => _select('year', year),
+                                      );
+                                    }
+                                  }).toList(),
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Expanded(
+                                  flex: 1,
+                                  child: ListView(
+                                    scrollDirection: Axis.vertical,
+                                    children: months.map((month) {
+                                      bool isSelected = month.toString() == DateFormat('M').format(DateTime.fromMillisecondsSinceEpoch(widget.current));
+                                      if (isSelected) {
+                                        return ElevatedButton(
+                                          style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                                          child: Text(month.toString() + '월',
+                                              style: TextStyle(fontSize: 16, color: Colors.white)
+                                          ),
+                                          onPressed: () => _select('month', month),
+                                        );
+                                      } else {
+                                        return TextButton(
+                                          child: Text(month.toString() + '월',
+                                              style: TextStyle(fontSize: 16, color: Colors.black)
+                                          ),
+                                          onPressed: () => _select('month', month),
+                                        );
+                                      }
+                                    }).toList(),
+                                  )
+                              )
+                            ],
+                          )),
+                          SizedBox(height: 16),
+                          Container(
+                            child: (
+                                ElevatedButton(
+                                    style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFFFD600), minimumSize: Size.fromHeight(50)),
+                                    child: Text('확인', style: TextStyle(color: Colors.black, fontSize: 18),),
+                                    onPressed: () => widget.select(widget.current)
+                                )
+                            ),
+                          ),
+                        ],
+                      )
+                  ))
+              ),)
+        )
+    );
+  }
+}
+
