d0e14bea154e39781d8c6099d68dc90061412349
Albert <youngh0910@gmail.com>
2023-12-27 21:19:58 +0900

    feat: design system about font size

--- lib/design/font_size.dart
+++ lib/design/font_size.dart
@@ -1,4 +1,12 @@
 class FontSizeDesign {
-  static const double app_bar_title = 26;
-  static const double app_bar_title_detail = 17;
+  static const double LARGE_TITLE = 32;
+  static const double TITLE_1 = 23;
+  static const double TITLE_2 = 22;
+  static const double TITLE_3 = 20;
+  static const double HEADLINE = 18;
+  static const double BODY_1 = 18;
+  static const double BODY_2 = 16;
+  static const double BODY_3 = 14;
+  static const double CAPTION_1 = 13;
+  static const double CAPTION_2 = 12;
 }
