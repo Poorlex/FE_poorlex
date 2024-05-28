48b7fbc22df12c50f564a1b698bc72f7887bdac4
rogan <jh241093@gmail.com>
2024-05-28 23:52:25 +0900

    delete: base.dart는 불필요한 Widget입니다.

--- lib/screen/calendar/calendar_page.dart
+++ lib/screen/calendar/calendar_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:poorlex/libs/theme.dart';
-import 'package:poorlex/widget/common/base.dart';
-
 import 'package:poorlex/widget/calendar/header.dart';
 import 'package:poorlex/widget/calendar/list.dart';
 
@@ -24,21 +22,25 @@ class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: CColors.black,
-        body: (SafeArea(
-            child: CCon(
-                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: CCol(
-                  children: [
-                    CalenderHeader(current: _current, select: select),
-                    Flexible(
-                        flex: 1,
-                        child: List(
-                          current: _current,
-                        ))
-                  ],
-                  cross: CrossAxisAlignment.start,
-                )))));
+      resizeToAvoidBottomInset: false,
+      backgroundColor: CColors.black,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+          child: Column(
+            children: [
+              CalenderHeader(current: _current, select: select),
+              Flexible(
+                flex: 1,
+                child: List(
+                  current: _current,
+                ),
+              )
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screen/login/login.dart
+++ lib/screen/login/login.dart
@@ -1,11 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'dart:io';
 
-import 'package:poorlex/widget/common/base.dart';
+import 'package:flutter/material.dart';
+
+import 'package:poorlex/controller/apple_auth.dart';
+import 'package:poorlex/controller/kakao_auth.dart';
+
 import 'package:poorlex/widget/common/buttons.dart';
 
 import 'package:poorlex/libs/theme.dart';
-import 'package:poorlex/controller/user.dart';
 
 class Login extends StatefulWidget {
   Login({
@@ -17,82 +19,111 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final user = Get.find<UserController>();
+  final _kakaoAuthController = KaKaoAuthController();
+  final _appleAuthController = AppleAuthController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: CColors.white,
-        resizeToAvoidBottomInset: false,
-        body: SafeArea(
-            child: CCon(
-                color: CColors.white,
-                padding: EdgeInsets.all(20),
-                child: CCol(
-                    main: MainAxisAlignment.center,
-                    cross: CrossAxisAlignment.center,
+      backgroundColor: CColors.white,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Container(
+          color: CColors.white,
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/login/login.png',
+                width: 300,
+              ),
+              SizedBox(height: 41),
+              Text(
+                '풀렉스에서 다른 거지 친구들과 함께\n재미있게 절약해봐요!',
+                style: CTextStyles.Body2(
+                  color: CColors.black,
+                  height: 1.4,
+                  fontFamily: 'appleSDGothicNeo',
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 41),
+              CButton(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                type: ButtonTypes.elevated,
+                color: CColors.kakaoYellow,
+                child: Container(
+                  height: 56,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Container(
-                          width: 300,
-                          child: Image.asset('assets/login/login.png')),
-                      SizedBox(height: 41),
-                      Text('풀렉스에서 다른 거지 친구들과 함께\n재미있게 절약해봐요!',
-                          style: CTextStyles.Body2(
-                              color: CColors.black,
-                              height: 1.4,
-                              fontFamily: 'appleSDGothicNeo'),
-                          textAlign: TextAlign.center),
-                      SizedBox(height: 41),
-                      CButton(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          type: ButtonTypes.elevated,
-                          color: CColors.kakaoYellow,
-                          child: Container(
-                            height: 56,
-                            child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Image.asset(
-                                    'assets/login/kakao.png',
-                                    width: 24,
-                                    height: 24,
-                                  ),
-                                  Text('카카오로 3초만에 로그인',
-                                      style: CTextStyles.Body2(
-                                          fontWeight: FontWeight.w600,
-                                          color: CColors.black,
-                                          fontFamily: 'appleSDGothicNeo')),
-                                  SizedBox(width: 24)
-                                ]),
-                          ),
-                          onPressed: () => Get.toNamed('/login/kakao')),
-                      SizedBox(height: 20),
-                      CButton(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          type: ButtonTypes.elevated,
+                      Image.asset(
+                        'assets/login/kakao.png',
+                        width: 24,
+                        height: 24,
+                      ),
+                      Text(
+                        '카카오로 3초만에 로그인',
+                        style: CTextStyles.Body2(
+                          fontWeight: FontWeight.w600,
                           color: CColors.black,
-                          child: Container(
-                            height: 56,
-                            child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Image.asset(
-                                    'assets/login/apple.png',
-                                    width: 24,
-                                    height: 24,
-                                  ),
-                                  Text('Apple ID로 로그인',
-                                      style: CTextStyles.Body2(
-                                          fontWeight: FontWeight.w600,
-                                          fontFamily: 'appleSDGothicNeo')),
-                                  SizedBox(width: 24)
-                                ]),
+                          fontFamily: 'appleSDGothicNeo',
+                        ),
+                      ),
+                      SizedBox(width: 24)
+                    ],
+                  ),
+                ),
+                onPressed: () async {
+                  // Get.toNamed('/login/kakao');
+                  final socialLoginModel =
+                      await _kakaoAuthController.kakaoLogin();
+                  print(">>>>>>> $socialLoginModel");
+                },
+              ),
+              if (Platform.isIOS) ...[
+                SizedBox(height: 20),
+                CButton(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  type: ButtonTypes.elevated,
+                  color: CColors.black,
+                  child: Container(
+                    height: 56,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/login/apple.png',
+                          width: 24,
+                          height: 24,
+                        ),
+                        Text(
+                          'Apple ID로 로그인',
+                          style: CTextStyles.Body2(
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'appleSDGothicNeo',
                           ),
-                          onPressed: () => Get.toNamed('/login/apple')),
-                    ]))));
+                        ),
+                        SizedBox(width: 24)
+                      ],
+                    ),
+                  ),
+                  onPressed: () async {
+                    // Get.toNamed('/login/apple');
+                    final socialLoginModel = await _appleAuthController
+                      ..appleLogin();
+                    print(">>>>>>>> $socialLoginModel");
+                  },
+                ),
+              ]
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
