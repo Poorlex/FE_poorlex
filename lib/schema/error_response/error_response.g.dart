1a112fd24cd4c1b1d1b3d08a7d1fc840e9a87bbf
rogan <jh241093@gmail.com>
2024-06-16 18:31:35 +0900

    add: dartz 추가 및 배틀 참가 에러 처리 - 에러 알럿 띄위기 참고

---
+++ lib/schema/error_response/error_response.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'error_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ErrorResponseImpl _$$ErrorResponseImplFromJson(Map<String, dynamic> json) =>
+    _$ErrorResponseImpl(
+      tag: json['tag'] as String,
+      message: json['message'] as String,
+    );
+
+Map<String, dynamic> _$$ErrorResponseImplToJson(_$ErrorResponseImpl instance) =>
+    <String, dynamic>{
+      'tag': instance.tag,
+      'message': instance.message,
+    };
