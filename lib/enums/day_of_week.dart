8cd2aacae109ba75120ef9f1f493116efbc968ef
rogan <jh241093@gmail.com>
2024-06-17 22:29:30 +0900

    add: 요일 포맷 추가

--- lib/enums/day_of_week.dart
+++ lib/enums/day_of_week.dart
@@ -7,3 +7,48 @@ enum DayOfWeek {
   SATURDAY,
   SUNDAY,
 }
+
+/// 오늘 날짜의 DayOfWeek 선택
+DayOfWeek getTodayDayOfWeek() {
+  DateTime today = DateTime.now();
+  switch (today.weekday) {
+    case DateTime.monday:
+      return DayOfWeek.MONDAY;
+    case DateTime.tuesday:
+      return DayOfWeek.TUESDAY;
+    case DateTime.wednesday:
+      return DayOfWeek.WEDNESDAY;
+    case DateTime.thursday:
+      return DayOfWeek.THURSDAY;
+    case DateTime.friday:
+      return DayOfWeek.FRIDAY;
+    case DateTime.saturday:
+      return DayOfWeek.SATURDAY;
+    case DateTime.sunday:
+      return DayOfWeek.SUNDAY;
+    default:
+      throw Exception("Invalid day of week");
+  }
+}
+
+// DayOfWeek enum 값을 한글 문자열로 변환하는 함수
+String getKoreanDayOfWeek(DayOfWeek day) {
+  switch (day) {
+    case DayOfWeek.MONDAY:
+      return '월';
+    case DayOfWeek.TUESDAY:
+      return '화';
+    case DayOfWeek.WEDNESDAY:
+      return '수';
+    case DayOfWeek.THURSDAY:
+      return '목';
+    case DayOfWeek.FRIDAY:
+      return '금';
+    case DayOfWeek.SATURDAY:
+      return '토';
+    case DayOfWeek.SUNDAY:
+      return '일';
+    default:
+      throw Exception("Invalid day of week");
+  }
+}
