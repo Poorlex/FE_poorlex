6e8884b0a15e81e5861643b8b9830fd23f3e42d3
rogan <jh241093@gmail.com>
2024-06-19 18:01:11 +0900

    refactor: battleAppBar는 불필요한 파일 분리로 제거하였습니다.

--- lib/screen/battle/battle_ranking.dart
+++ lib/screen/battle/battle_ranking.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:poorlex/controller/battle_detail.dart';
 import 'package:poorlex/enums/day_of_week.dart';
-
-import 'package:poorlex/widget/battle/ranking_app_bar.dart';
+import 'package:poorlex/libs/theme.dart';
 import 'package:poorlex/widget/battle/ranking_detail.dart';
 
 class BattleRanking extends StatefulWidget {
@@ -35,8 +34,31 @@ class _BattleRankingState extends State<BattleRanking> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: RankingAppBarWidget(),
+      appBar: _appBar(),
       body: RankingDetailWidget(battleId: widget.battleId),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      backgroundColor: CColors.black,
+      centerTitle: true,
+      title:
+          Text('참여중', style: CTextStyles.Body1(), textAlign: TextAlign.center),
+      actions: [
+        IconButton(
+          onPressed: () async {
+            await Get.toNamed(
+              '/battle/modify/${widget.battleId}',
+            );
+            _battleDetail.getDetailById(
+              battleId: widget.battleId,
+            );
+          },
+          icon: Image.asset('assets/app_bar/icon_config_26_26.png'),
+          iconSize: 26,
+        ),
+      ],
     );
   }
 }
