c4f783de02786d2a253f5fdb3989501f6eeeb21f
lwyeol <lwyeol1010@azero.biz>
2024-06-23 01:11:41 +0900

    feat: 배틀방 만들기 캐러셀 클릭시 배틀 생성 페이지 이동

--- lib/widget/home/empty_battle.dart
+++ lib/widget/home/empty_battle.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:poorlex/libs/theme.dart';
 
-import 'package:poorlex/Libs/Theme.dart';
+import 'package:poorlex/screen/battle/battle_create.dart';
 
 class EmptyBattle extends StatelessWidget {
   const EmptyBattle({super.key});
@@ -11,46 +13,45 @@ class EmptyBattle extends StatelessWidget {
       builder: (context, constraints) {
         final double secondContainerWidth = constraints.maxWidth - 6;
 
-        return Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(color: CColors.black),
-              width: secondContainerWidth + 6,
-              height: 144,
-            ),
-            Container(
-              width: secondContainerWidth,
-              height: 150,
-              decoration: BoxDecoration(color: CColors.black),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  OutlinedButton(
-                    child: SizedBox(
+        return GestureDetector(
+          onTap: () {
+            /// [TODO] 예산 미설정 시 예산 설정 페이지로 라우팅
+            // 배틀 페이지로 이동
+            Get.to(() => BattleCreate());
+          },
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(color: CColors.blackOpa80),
+                width: secondContainerWidth + 6,
+                height: 144,
+              ),
+              Container(
+                width: secondContainerWidth,
+                height: 150,
+                decoration: BoxDecoration(color: CColors.blackOpa80),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 36),
+                    SizedBox(
                       width: 34,
                       height: 34,
                       child: Image.asset('assets/main_page/empty_battle.png'),
                     ),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(width: 0),
-                    ),
-                    onPressed: () {},
-                  ),
-                  OutlinedButton(
-                    child: Text(
+                    SizedBox(height: 12),
+                    Text(
                       '배틀 방 만들기',
-                      style: CTextStyles.Headline(color: CColors.gray70),
-                    ),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(width: 0),
-                    ),
-                    onPressed: () {},
-                  )
-                ],
+                      style: CTextStyles.Title3(
+                        color: CColors.gray70,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
